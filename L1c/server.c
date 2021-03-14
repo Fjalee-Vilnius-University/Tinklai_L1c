9164a34f3cafea3b0c2d52b8c4422acd16cce852
Rytis <cepRytis@gmail.com>
2021-03-14 15:42:25 +0200

    changed from poll server to selector server

--- L1c/server.c
+++ L1c/server.c
@@ -22,15 +22,6 @@ void error(char *msg){
     exit(1);
 }
 
-void sigchld_handler(int s)
-{
-    int saved_errno = errno;
-
-    while(waitpid(-1, NULL, WNOHANG) > 0);
-
-    errno = saved_errno;
-}
-
 void *get_in_addr(struct sockaddr *sa)
 {
     if (sa->sa_family == AF_INET) {
@@ -89,29 +80,6 @@ int get_listener_socket(void)
     return listener;
 }
 
-void add_to_pfds(struct pollfd *pfds[], int newfd, int *fd_count, int *fd_size)
-{
-    // Double pdfs space if its full
-    if (*fd_count == *fd_size) {
-        *fd_size *= 2;
-
-        *pfds = realloc(*pfds, sizeof(**pfds) * (*fd_size));
-    }
-
-    (*pfds)[*fd_count].fd = newfd;
-    (*pfds)[*fd_count].events = POLLIN;
-
-    (*fd_count)++;
-}
-
-void del_from_pfds(struct pollfd pfds[], int i, int *fd_count)
-{
-    // Copy the one from the end over this one
-    pfds[i] = pfds[*fd_count-1];
-
-    (*fd_count)--;
-}
-
 /*void strToUpper(char *str){
     for (int i; i <= strlen(str); i++){
         str[i] = toupper(str[i]);
@@ -124,6 +92,7 @@ int main(void){
     struct sockaddr_storage cli_addr;
     socklen_t addr_size;
     char buff[MAXLEN];
+    int nbytes;
     char remoteIP[INET6_ADDRSTRLEN];
 
     // Start off with room for 5 connections
@@ -132,63 +101,68 @@ int main(void){
     int fd_size = 5;
     struct pollfd *pfds = malloc(sizeof *pfds * fd_size);
 
+    fd_set master;
+    fd_set read_fds;
+    int fdmax;
+
+    FD_ZERO(&master);
+    FD_ZERO(&read_fds);
+
     listener = get_listener_socket();
-    pfds[0].fd = listener;
-    pfds[0].events = POLLIN;
-    fd_count = 1;
+    FD_SET(listener, &master);
+    fdmax = listener;
 
     for(;;) {
-        int poll_count = poll(pfds, fd_count, -1);
+        read_fds = master;
+        if(1 == select(fdmax+1, &read_fds, NULL, NULL, NULL)){
+            error("Failed selector...");
 
-        if (poll_count == -1)
-            error("Failed creating poll...\n");
+            // Run through the existing connections looking for data to read
+            for(int i = 0; i <= fdmax; i++) {
+                if(FD_ISSET(i, &read_fds)){
+                    if(i == listener){
+                        addr_size = sizeof cli_addr;
+                        new_fd = accept(listener, (struct sockaddr *)&cli_addr, &addr_size);
 
-        // Run through the existing connections looking for data to read
-        for(int i = 0; i < fd_count; i++) {
-            if (pfds[i].revents & POLLIN) {
+                        if (new_fd == -1) 
+                            error("Acepting failed ...\n");
+                        else {
+                            FD_SET(new_fd, &master);
+                            if(new_fd > fdmax)
+                                fdmax = new_fd;
 
-                if (pfds[i].fd == listener) { // If listener is ready to read, handle new connection
-                
-                    addr_size = sizeof cli_addr;
-                    new_fd = accept(listener, (struct sockaddr *)&cli_addr, &addr_size);
-
-                    if (new_fd == -1) 
-                        error("Acepting failed ...\n");
-                    else {
-                        add_to_pfds(&pfds, new_fd, &fd_count, &fd_size);
-
-                        printf("pollserver: new connection from %s on "
-                            "socket %d\n",
-                            inet_ntop(cli_addr.ss_family,
-                                get_in_addr((struct sockaddr*)&cli_addr),
-                                remoteIP, INET6_ADDRSTRLEN),
-                            new_fd);
+                            printf("New connection from %s on "
+                                "socket %d\n",
+                                inet_ntop(cli_addr.ss_family,
+                                    get_in_addr((struct sockaddr*)&cli_addr),
+                                    remoteIP, INET6_ADDRSTRLEN),
+                                new_fd);
+                        }
                     }
-                }
 
-                else {    // If not the listener, we're just a regular client
-                    int nbytes = recv(pfds[i].fd, buff, sizeof buff, 0);
-                    int sender_fd = pfds[i].fd;
+                    else {    // If not the listener, we're just a regular client
+                        int nbytes = recv(pfds[i].fd, buff, sizeof buff, 0);
+                        int sender_fd = pfds[i].fd;
 
-                    if (nbytes <= 0) {    // Got error or connection closed by client
+                        if (nbytes <= 0) {    // Got error or connection closed by client
+                            // Connection closed
+                            if (nbytes == 0)    
+                                printf("pollserver: socket %d hung up\n", sender_fd);
+                            else 
+                                error("Error receiving...\n");
 
-                        // Connection closed
-                        if (nbytes == 0)    
-                            printf("pollserver: socket %d hung up\n", sender_fd);
-                        else 
-                            error("Error receiving...\n");
-
-                        close(pfds[i].fd);
-                        del_from_pfds(pfds, i, &fd_count);
-                    } 
-                    else {
-                        for(int j = 0; j < fd_count; j++) {    // Send to everyone
-                            int dest_fd = pfds[j].fd;
-
-                            // Except the listener and ourselves
-                            if (dest_fd != listener && dest_fd != sender_fd) {
-                                if (send(dest_fd, buff, nbytes, 0) == -1) {
-                                    error("Error sending...\n");
+                            close(i);
+                            FD_CLR(i, &master);
+                        } 
+                        else {
+                            for(int j = 0; j < fd_count; j++) {    // Send to everyone
+                                if(FD_ISSET(j, &master)){
+                                    // Except the listener and ourselves
+                                    if (j != listener && j != i) {
+                                        if (send(j, buff, nbytes, 0) == -1) {
+                                            error("Error sending...\n");
+                                        }
+                                    }
                                 }
                             }
                         }
